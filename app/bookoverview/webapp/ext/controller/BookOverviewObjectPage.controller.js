6a840808cc89be6e680c2602b2166c5b29426faa
Wonjun Choi <wonjun.choi@lt-s.jp>
2026-06-20 00:46:27 +0000

    fix3

--- app/bookoverview/webapp/ext/controller/BookOverviewObjectPage.controller.js
+++ app/bookoverview/webapp/ext/controller/BookOverviewObjectPage.controller.js
@@ -1,25 +1,15 @@
 sap.ui.define([], function () {
     "use strict";
 
-    function getObjectPageContext(oEvent) {
-        const oSource = oEvent && oEvent.getSource && oEvent.getSource();
-
-        return oSource && oSource.getBindingContext && oSource.getBindingContext();
-    }
-
-    function buildBookUrl(oContext) {
-        const sTitle = oContext && oContext.getProperty("TITLE");
-        const sAuthor = oContext && oContext.getProperty("AUTHOR_NAME");
-        const sBookId = oContext && oContext.getProperty("BOOK_ID");
-        const sQuery = [sTitle, sAuthor || sBookId].filter(Boolean).join(" ");
-
-        // eslint-disable-next-line @sap-ux/fiori-tools/sap-no-hardcoded-url
-        return "https://www.google.com/search?q=" + encodeURIComponent(sQuery || "book");
-    }
-
     return {
-        onOpenBookLink: function (oEvent) {
-            window.open(buildBookUrl(getObjectPageContext(oEvent)), "_blank", "noopener,noreferrer");
+        onOpenBookLink: function () {
+            // eslint-disable-next-line @sap-ux/fiori-tools/sap-no-hardcoded-url
+            const sUrl = "https://www.google.com/search?q=book";
+            const oWindow = window.open(sUrl, "_blank", "noopener,noreferrer");
+
+            if (!oWindow) {
+                window.location.href = sUrl;
+            }
         }
     };
 });
